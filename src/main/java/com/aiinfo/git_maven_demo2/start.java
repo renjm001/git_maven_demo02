07e575445255e566254248a60a8d85b2ec496cf6
renjm001 <1181667260@qq.com>
2022-06-22 17:08:05 +0800

    这是demo3修改 冲突已解决222

--- src/main/java/com/aiinfo/git_maven_demo2/start.java
+++ src/main/java/com/aiinfo/git_maven_demo2/start.java
@@ -8,6 +8,7 @@ package com.aiinfo.git_maven_demo2;
 public class start {
     public static void main(String[] args) {
         System.out.println("这是测试！！！！");
+        System.out.println("hahaha");
         System.out.println("这是demo3项目修改");
         System.out.println("这是demo2项目修改!!!");
     }

06445ee6ec6e95afe22b0fee943bcd57dc906e4e
renjm001 <1181667260@qq.com>
2022-06-22 11:59:08 +0800

    git的maven项目第一次提交！！！！

---
+++ src/main/java/com/aiinfo/git_maven_demo2/start.java
@@ -0,0 +1,12 @@
+package com.aiinfo.git_maven_demo2;
+
+/**
+ * @Description:
+ * @Author: renjm
+ * @date: 2022/6/22
+ */
+public class start {
+    public static void main(String[] args) {
+        System.out.println("这是测试！！！！");
+    }
+}
